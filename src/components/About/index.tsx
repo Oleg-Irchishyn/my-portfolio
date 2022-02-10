36f52642191a1d4236ff37b57395f5886af07287
Oleg Irchishyn <o.irchishyn@smileexpo.com.ua>
2022-02-10 19:11:28 +0200

    updating project

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from './../../styles/components/About.module.scss';
 import { ReactTypical } from '@deadcoder0904/react-typical';
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 import cn from 'classnames';
+import 'overlayscrollbars/css/OverlayScrollbars.css';
 
 const About: React.FC = () => {
+ 
   return (
     <section id="about" className={cn(styles.about)}>
       <div className={cn(styles.about__top_content, 'container')}>
@@ -21,6 +24,18 @@ const About: React.FC = () => {
           loop={Infinity}
           wrapper="div"
         />
+      </div>
+      <div className={cn(styles.about__bottom_content, 'container')}>
+        <OverlayScrollbarsComponent >
+      
+          Outsource company or a product one is not a concern to me. I am looking for the company,
+          that would give me an opportunity to grow and learn. The most important moment for me is
+          communication within the development team and clients. Bad management causes a lot of
+          issues, as well as bad code. Whether I will be coding websites or web services, all I want
+          is to sharpen my skills and learn JS and ReactJS on real projects. The end goal is to
+          become an outstanding developer, who will participate in difficult projects, which will
+          bring profit to a company and satisfaction to me.
+        </OverlayScrollbarsComponent>
       </div>
     </section>
   );
