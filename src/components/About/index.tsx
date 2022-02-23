96b7361071f1f12a84028c86fe544bebe23a509e
Oleg-Irchishyn <oleg.irchishyn91@gmail.com>
2022-02-23 15:41:37 +0200

    updated my portfolio project

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -31,22 +31,12 @@ const About: React.FC = React.memo(() => {
       <div className={cn(styles.about__bottom_content, 'container')}>
         <ImageSlider />
         <OverlayScrollbarsComponent >
-          <p data-aos="flip-down" data-aos-duration="500"> Outsource company or a product one is not a concern to me.</p>
-          <p data-aos="flip-down" data-aos-duration="500">I am looking for the company, that would give me an opportunity to grow and learn. </p>
-          <p data-aos="flip-down" data-aos-duration="500">
-            {' '}
-            The most important moment for me is communication within the development team and
-            clients.{' '}
-          </p>
-          <p data-aos="flip-down" data-aos-duration="500">Bad management causes a lot of issues, as well as bad code. </p>
-          <p data-aos="flip-down" data-aos-duration="500">
-            Whether I will be coding websites or web services, all I want is to sharpen my skills
-            and learn JS and ReactJS on real projects.{' '}
-          </p>
-          <p data-aos="flip-down" data-aos-duration="500">
-            The end goal is to become an outstanding developer, who will participate in difficult
-            projects, which will bring profit to a company and satisfaction to me.
-          </p>
+          <p data-aos="flip-down" data-aos-duration="500"> {t('aboutText.1')}</p>
+          <p data-aos="flip-down" data-aos-duration="500">{t('aboutText.2')}</p>
+          <p data-aos="flip-down" data-aos-duration="500"> {t('aboutText.3')}</p>
+          <p data-aos="flip-down" data-aos-duration="500">{t('aboutText.4')}</p>
+          <p data-aos="flip-down" data-aos-duration="500">{t('aboutText.5')}</p>
+          <p data-aos="flip-down" data-aos-duration="500">{t('aboutText.6')}</p>
         </OverlayScrollbarsComponent>
       </div>
     </section>
